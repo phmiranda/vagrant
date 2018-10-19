554992459d0177635712df448bc23b8f05683b50
pedro.miranda <pehhagah.1607@gmail.com>
2018-10-18 21:21:02 -0300

    atualização nos prividers locais no vagrant file e shell script.

---
+++ ubuntu/config/atualizacao.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+# comentários.
+echo atualizando os repositórios...
+if ! sudo apt-get update
+then
+    echo "Foram identificados erros na atualização dos repositórios, favor tentar novamente."
+    exit 1
+fi
+    echo "Atualização feita com sucesso."
--- ubuntu/config/puppet.sh
+++ ubuntu/config/puppet.sh
@@ -1,13 +1,3 @@
-﻿#!/bin/bash
-# comentários.
-echo atualizando os repositórios...
-if ! sudo apt-get update
-then
-    echo "Foram identificados erros na atualização dos repositórios, favor tentar novamente."
-    exit 1
-fi
-    echo "Atualização feita com sucesso."
-    
 # comentários.
 echo instalando automatizador de deploy...
 if ! sudo apt-get install -y puppet
